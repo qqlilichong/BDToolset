bd5eb58aa64eea1d2e4c47f57ac8abbc0b6b9756
ric <qqlilichong@163.com>
2018-04-12 14:13:04 +0800

    commit config.

---
+++ enc/enc_aac.py
@@ -0,0 +1,87 @@
+
+#################################################################################
+
+
+import os
+import sys
+import subprocess
+
+
+#################################################################################
+
+
+tool = {
+    'BDTOOL': 'E:/gitpri/BDToolset',
+    'FFMPEG': 'ffmpeg/ffhi.exe',
+}
+
+ffmpeg = [
+    '-c:v copy',
+    '-c:a libfdk_aac',
+    '-vbr 4',
+]
+
+
+#################################################################################
+
+
+# media file.
+tool['SOURCE'] = input('media file : ')
+
+output = os.path.join(os.path.dirname(tool['SOURCE']), 'ffaac_' + os.path.basename(tool['SOURCE']))
+
+# QAAC params.
+ffmpeg.append('"%s"' % output)
+
+
+#################################################################################
+
+
+# fix path.
+for x, y in tool.items():
+    if os.path.exists(y):
+        continue
+    y = os.path.join(tool['BDTOOL'], y)
+    if not os.path.exists(y):
+        print('missing : %s' % y)
+        sys.exit(0)
+    tool[x] = y
+
+for x, y in tool.items():
+    tool[x] = '"' + y + '"'
+
+
+#################################################################################
+
+
+# make command.
+command = [
+    tool['FFMPEG'],
+    '-i',
+    tool['SOURCE'],
+]
+
+command += ffmpeg
+command = ' '.join(line for line in command)
+
+
+#################################################################################
+
+
+# setup encoding.
+print('')
+print(command)
+print('')
+
+# clear old file.
+if os.path.exists(output):
+    os.remove(output)
+
+# encoding...
+subprocess.call(command, shell=True)
+
+# end.
+os.system('pause')
+
+
+#################################################################################
